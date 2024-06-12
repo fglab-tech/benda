bfe4ec0b960ce631a9766f8090ed094d4c34f712
vkobinski <victorkobinski@outlook.com>
2024-06-12 20:30:40 -0300

    Updated examples

--- examples/simple.py
+++ examples/simple.py
@@ -2,13 +2,12 @@ import benda
 from benda import bjit, u24
 
 
+@bjit
 def simple() -> u24:
     x = u24(3)
     y = x - u24(2)
     return y
 
 
-if __name__ == "__main__":
-    translated_simple = bjit(simple)
-    print(simple())
-    print(translated_simple())
+val = simple()
+print(val)
