fd30af7596e5cb05e2d6eb125628395b36f0721e
Kelvin Steiner <me@steinerkelvin.dev>
2024-06-18 07:37:41 -0300

    moving transpilation examples

--- examples/future/fib.py
+++ examples/future/fib.py
@@ -1,21 +1,10 @@
 from typing import TypeVar
-# from benda import bjit
+from benda import bjit, bend
 
 T = TypeVar("T")
 
 
-def bend(fn: T) -> T:
-    """
-    Decorator that hints the transpiler that a call to the recursive function
-    should be translated to an inline `bend` block.
-
-    This decorator is intended to be defined internally by the benda library.
-    """
-    return fn
-
-# @bjit
-
-
+@bjit
 def fib_recursive(n: int) -> int:
     """
     Calculates the nth Fibonacci number using a recursive approach.
@@ -28,16 +17,15 @@ def fib_recursive(n: int) -> int:
         case _:
             return fib_recursive(n - 2) + fib_recursive(n - 1)
 
-# @bjit
 
-
+@bjit
 def fib_iterative(n: int) -> int:
     """
     Calculates the nth Fibonacci number using an iterative (tail-recursive) approach.
     """
 
-    # This decorator does nothing, it just hints the benda transpiler to
-    # use a `bend` block for the recursive computation.
+    # This decorator hints the Benda transpiler to use a `bend` block for the
+    # recursive computation.
     @bend
     def go(a: int, b: int, n: int) -> int:
         if n == 0:
--- examples/future/tree.py
+++ examples/future/tree.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+import random
+
 from benda import bjit, u24
+
 
 @dataclass
 class MyLeaf:
@@ -24,14 +27,15 @@ def sum_tree(tree: MyTree) -> u24:
             return sum_tree(left) + sum_tree(right)
 
 
-def gen_tree(depth: int, n: int) -> MyTree:
+def gen_tree(depth: int) -> MyTree:
+    n = random.randint(0, 1000)
     if depth == 0:
         return MyLeaf(value=n)
     else:
-        return MyNode(left=gen_tree(depth-1, n-1), right=gen_tree(depth-1, n+1))
+        return MyNode(left=gen_tree(depth-1), right=gen_tree(depth-1))
 
 
 if __name__ == "__main__":
-    tree = gen_tree(4, 10)
+    tree = gen_tree(4)
     print(tree)
     print(sum_tree(tree))
